1661a6ad11ccaec28624faa61da881bc5b1f24f8
Denis <denislupookov@gmail.com>
2021-11-29 23:22:43 +0200

    Added Sidebar to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Router from './routes/Router'
 import Header from './app/layout/Header'
+import Sidebar from './app/layout/Sidebar'
 import './assets/css/style.css'
 
 function App() {
   return (
-    <div>
+    <div style={{display: "flex"}}>
       <Header />
+      <Sidebar />
       <Router />
     </div>
   );

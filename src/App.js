c55d88cb6d3e9f0eb37ea246e5d2eb3b0082d21f
Denis <denislupookov@gmail.com>
2021-11-29 23:05:29 +0200

    Added Header to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Router from './routes/Router'
+import Header from './app/layout/Header'
 import './assets/css/style.css'
 
 function App() {
   return (
     <div>
+      <Header />
       <Router />
     </div>
   );
